9e0070fcfc0c074aaf8d8271bf382f4e2614d2f1
Slson <36367068+Slson@users.noreply.github.com>
2018-03-16 16:34:51 +0200

    Add files via upload

---
+++ OOP5Hometsk1.java
@@ -0,0 +1,13 @@
+package oop5hometsk1;
+
+public class OOP5Hometsk1  {
+
+    public static void main(String[] args) {
+        Copy rez=new Copy();
+        
+        rez.copyFile(rez.getFilesToCopy("zip"));
+       
+
+    }
+
+}
