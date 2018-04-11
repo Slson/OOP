6c68487bd23b899c42d026025a1964c7bf5dd428
Slson <36367068+Slson@users.noreply.github.com>
2018-04-11 14:33:49 +0300

    Update BlackList.java

--- BlackList.java
+++ BlackList.java
@@ -3,15 +3,4 @@ public class BlackList extends Conteiner{
 
     public BlackList() {
     }
-    
-    
-//    public boolean checkIfClassInBlackList(Class classToCheck){
-//        for (int i = 0; i < temp; i++) {
-//            if (classToCheck.equals(getClassArray()[i])){
-//                return true;
-//            }
-//        }
-//        return false;
-//        
-//    }
 }
