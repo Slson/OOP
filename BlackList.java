14f95750235495d1f0ab48ab63095c5fbdb05080
Slson <36367068+Slson@users.noreply.github.com>
2018-04-11 14:45:04 +0300

    Update BlackList.java

--- BlackList.java
+++ BlackList.java
@@ -3,4 +3,16 @@ public class BlackList extends Conteiner{
 
     public BlackList() {
     }
+       @Override
+    public boolean checkIfExest(Class clasToCheck) {
+        boolean tmp=false;
+        for (int i = 0; i < temp; i++) {
+            if (getClassArray()[i].equals(clasToCheck)) {
+                System.out.println("has not been written. " + clasToCheck.getName() + " this class is in a Black List");
+                tmp=true;
+            }
+        }
+        return tmp;
+      
+    }
 }
