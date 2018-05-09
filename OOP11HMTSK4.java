4ef9563ccb550949133532c87b5775f5cbe7793b
Slson <36367068+Slson@users.noreply.github.com>
2018-05-09 16:43:25 +0300

    Update OOP11HMTSK4.java

--- OOP11HMTSK4.java
+++ OOP11HMTSK4.java
@@ -8,8 +8,9 @@ public class OOP11HMTSK4 {
 
     public static void main(String[] args) throws IOException {
 
-        String answer = "<a href=\"1\">PRESS1</a><a href=\"2\">PRESS2</a><a href=\"3\">PRESS3</a><a href=\"4\">PRESS4</a><a href=\"5\">PRESS5</a><a href=\"/\">PRESS/</a><a href=\"*\">PRESS*</a><a href=\"-\">PRESS-</a><a href=\"+\">PRESS+</a><a href=\"enter\">PRESS ENTER</a>";
-        Calculator calculator = new Calculator();
+      String answer = "<a href=\"1\">PRESS1</a><br/><a href=\"2\">PRESS2</a><br/><a href=\"3\">PRESS3</a><br/><a href=\"4\">PRESS4</a><br/><a href=\"5\">PRESS5</a><br/>"
+                + "<a href=\"/\">PRESS/</a><br/><a href=\"*\">PRESS*</a><br/><a href=\"-\">PRESS-</a><br/><a href=\"+\">PRESS+</a><br/><a href=\"enter\">PRESS ENTER</a>";
+           Calculator calculator = new Calculator();
         try (ServerSocket soc = new ServerSocket(80)) {
             for (;;) {
                 Socket clisoc = soc.accept();
